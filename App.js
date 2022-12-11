5ab41c45e7e432a31eefaa343da606812d160c42
Kadir Engin <kadirengin624@hotmail.com>
2022-12-11 21:35:33 +0300

    add general image picker structure.

--- App.js
+++ App.js
@@ -1,14 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+const App = () => {
+  const [image, setImage] = useState(null)
+
+  const pickImage = async () => {
+    
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!image 
+      ?
+        <Pressable
+          style={styles.button}
+          onPress={pickImage} 
+        >
+          <Text style={styles.buttonText}>Pick Image</Text>
+        </Pressable>
+      :
+        <View>
+          <View style={styles.imageView}>
+            <Image style={styles.image} source={{ uri: image }}/>
+          </View>
+          <View>
+            <Pressable
+              style={styles.button}
+              onPress={pickImage} 
+              title="Pick Another Image" 
+            >
+              <Text style={styles.buttonText}>Pick Another Image</Text>
+            </Pressable>
+            <Pressable 
+              style={styles.button}
+              title="Confirm Image" 
+            >
+              <Text style={styles.buttonText}>Confirm Image</Text>
+            </Pressable>
+          </View>
+        </View>
+      }
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +66,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: 'blue',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  imageView:{
+    margin: 25,
+    backgroundColor: "black",
+    borderWidth: 2,
+    borderRadius: 20
+  },
+  image:{
+    width: 360,
+    height: 270,
+    borderRadius: 20
+  }
 });
